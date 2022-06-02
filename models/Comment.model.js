7db6a3ea1a6ea572beb6687e8fa0e1b88a5638c7
FranciscoGBento <f_m_bento@hotmail.com>
2022-06-02 09:58:44 +0100

    User and comment models

---
+++ models/Comment.model.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema({
+  name: { type: String, required: true },
+  img: { type: String, default: "img" },
+  comment: { type: String },
+  author: { type: String },
+});
+
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,12 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
+    profileType: { type: String, enum: ["admin", "user"], default: "user" },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    comments: { type: Schema.Types.ObjectId, ref: "Comment" },
+    establishments: [{ type: Schema.Types.ObjectId, ref: "Establishment" }],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
