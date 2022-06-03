00a8b756fd875accad4636ac4d507ff1ebe8d04b
FranciscoGBento <f_m_bento@hotmail.com>
2022-06-03 16:51:45 +0100

    comments

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -4,24 +4,26 @@ const Comment = require("../models/Comment.model");
 const Establishment = require("../models/Establishment.model");
 const User = require("../models/User.model");
 
-router.post("/comments", (req, res, next) => {
-  const { name, img, comments, author, establishmentId } = req.body;
+router.post("/categories/type/:id/comment", (req, res, next) => {
+  const { name, comments } = req.body;
+  const {id} = req.params;
+  const { _id } = req.payload;
 
-  Comment.create({ name, img, comments, author })
+  Comment.create({ name, comments, author: _id })
     .then((newComment) => {
       return Establishment.findByIdAndUpdate(
-        establishmentId,
+        id,
         { $push: { comments: newComment._id } },
         { new: true }
       );
     })
     .then((newComment) => {
-        return User.findByIdAndUpdate(
-          author,
-          { $push: { comments: newComment._id } },
-          { new: true }
-        );
-      })
+      return User.findByIdAndUpdate(
+        _id,
+        { $push: { comments: newComment._id } },
+        { new: true }
+      );
+    })
     .then((response) => res.status(200).json(response))
     .catch((err) =>
       res.status(400).json({ message: "No comment added. Please, try again." })
