f1f1184374dc4b1e7546468c8880a55b54bba550
pedrocncosta <pedrocncosta@gmail.com>
2022-06-07 11:54:34 +0100

    add claudinary to server

--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,11 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    imageUrl: {
+      type: String,
+      default:
+        "https://i.pinimg.com/236x/ee/33/bf/ee33bf536025fc8cd7f719ff84fb7a43--bob-the-builder-cake-top-websites.jpg",
+    },
     profileType: { type: String, enum: ["admin", "user"], default: "user" },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
 const User = require("../models/User.model");
+const fileUploader = require("../config/cloudinary.config");
+
 
 router.get("/users", (req, res, next) => {
   User.find({})
@@ -22,6 +24,21 @@ router.get("/user/:userId", (req, res, next) => {
       res.json(user);
     })
     .catch(() => console.log("oi"));
+});
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ fileUrl: req.file.path });
 });
 
 router.put("/profile/:userId", (req, res, next) => {
