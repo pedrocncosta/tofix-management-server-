36796f568da3e81334b62bdc45581ac0bea5e9b8
pedrocncosta <pedrocncosta@gmail.com>
2022-06-07 12:34:16 +0100

    add claudinary to server2

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -28,16 +28,16 @@ router.get("/user/:userId", (req, res, next) => {
 
 // POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
 router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-  // console.log("file is: ", req.file)
- 
+  console.log("file is: ", req.file);
+
   if (!req.file) {
     next(new Error("No file uploaded!"));
     return;
   }
-  
+
   // Get the URL of the uploaded file and send it as a response.
   // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
-  
+
   res.json({ fileUrl: req.file.path });
 });
 
